89bf6d91576d775783b5d3c28e05cd9d1b4db75f
Suryalall <surysrahoo@gmail.com>
2026-03-02 16:29:23 -0300

    feat(order-service): add-persistency

---
+++ order-service/src/main/java/order_service/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package order_service.repository;
+
+import order_service.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, UUID> {}
---
+++ order-service/src/test/resources/sql/test.sql
@@ -0,0 +1,7 @@
+use edosdb;
+
+select * from orders;
+select * from orderitems;
+
+drop table orderitems;
+drop table orders;
